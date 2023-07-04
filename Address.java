6c293531560f510794c3faa9f4083f376f78bbd8
Bensadia Omar <128587580+Omar-938@users.noreply.github.com>
2023-07-04 15:51:08 +0200

    Add files via upload

---
+++ Address.java
@@ -0,0 +1,8 @@
+public class Address{
+
+    private int number;
+    private String street;
+    private String city;
+    private String country;
+
+}
---
+++ User.java
@@ -0,0 +1,10 @@
+import java.sql.Date;
+
+public class User{
+
+private Date birthdate;
+private int size;
+private String[] photos;
+private String address;
+
+}
